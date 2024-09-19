ebb21cbc361ded166667130619128027d888b546
alibasit <alibasit78@gmail.com>
2024-09-20 02:29:27 +0530

    updated the files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+setup(
+    name="NER Project",
+    version="0.0.0",
+    author="Basit",
+    author_email="alibasit78@gmail.com",
+    packages=find_packages(),
+)
